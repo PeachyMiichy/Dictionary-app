8192fb61a68384753a23df14ae83617b89c3e9d5
PeachyMiichy <michellevong97@gmail.com>
2022-06-22 21:44:07 +0800

    API Intergration
    Installed axios and added in the API URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,23 @@
 import { React, useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
+
+//https://api.dictionaryapi.dev/api/v2/entries/en/hello
 
 export default function Dictionary() {
   let [word, setWord] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
 
   function updateWord(event) {
     setWord(event.target.value);
   }
   function search(event) {
     event.preventDefault();
-    alert(word);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="Dictionary">
