5e490a7b3229ab7128d59e7deac8124d3643b528
PeachyMiichy <michellevong97@gmail.com>
2022-06-24 21:31:03 +0800

    CSS
    Addition of CSS files to make the dictionary more reader friendly

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,13 @@
 import { React, useState } from "react";
 import Meaning from "./Meaning";
+import "./Results.css";
 
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
       <div className="results">
-        <h2>{props.results.word}</h2>
+        <h2 className="keyword">{props.results.word}</h2>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
