e2da9a10207d379a7e16a82577b576459812854f
PeachyMiichy <michellevong97@gmail.com>
2022-07-02 17:21:03 +0800

    beautification

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,30 +6,47 @@ import Results from "./Results.js";
 //https://api.dictionaryapi.dev/api/v2/entries/en/hello
 
 export default function Dictionary() {
-  let [word, setWord] = useState("");
+  let [word, setWord] = useState("hello");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateWord(event) {
     setWord(event.target.value);
   }
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
   }
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={updateWord} />
-      </form>
-      <div className="Hint">Suggested search: Hello, Books, Magic...</div>
 
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <div className="search">
+          <h3>What word do you want to look up?</h3>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={updateWord} />
+          </form>
+          <div className="Hint">Suggested search: Hello, Books, Magic...</div>
+        </div>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
 }
